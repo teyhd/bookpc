445fe63b0c9e796d8e6559d861690c091b109386
vladteyhd <vladteyhd@mail.ru>
2024-01-12 00:23:16 +0300

    addapp

---
+++ client/Security/Security/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Security
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(466, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(706, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите пароль, чтобы продолжить работу";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(652, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "60";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(263, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 41);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(785, 163);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизуйтесь, пожалуйста";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ client/Security/Security/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Management;
+using System.ServiceProcess;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Security
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        /// 
+        public static int sec = 60;
+        private static Service1 MService;
+
+        [STAThread]
+        static async Task Main()
+        {
+            MService = new Service1();
+           // MService.StartService();
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MyBackgroundService>();
+                });
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Form1());
+
+            // Service1.Run();
+            // Security.Service1.
+            //Security.Service1.
+
+
+
+            await builder.RunConsoleAsync();
+        }
+
+        public static void LockWorkStation()
+        {
+            Process.Start("rundll32.exe", "user32.dll,LockWorkStation");
+        }
+
+        public static EventLogEntry GetLastLogonEvent(EventLog securityLog)
+        {
+            EventLogEntry lastLogonEvent = null;
+
+            // Проход по всем записям в журнале
+            foreach (EventLogEntry entry in securityLog.Entries)
+            {
+                // Проверка события входа в систему (EventID=4624)
+                if (entry.EventID == 4624)
+                {
+                    // Если это первое событие входа в систему или более позднее по времени, запоминаем его
+                    if (lastLogonEvent == null || entry.TimeGenerated > lastLogonEvent.TimeGenerated)
+                    {
+                        lastLogonEvent = entry;
+                    }
+                }
+            }
+
+            return lastLogonEvent;
+        }
+
+        static string GetUsernameFromLogonEvent(EventLogEntry logonEvent)
+        {
+            // В данном примере, мы предполагаем, что имя пользователя содержится в поле "Message" события
+            // В реальном приложении, вам может потребоваться использовать регулярные выражения или другие методы обработки строки
+            return logonEvent?.Message ?? "Не удалось определить имя пользователя.";
+        }
+
+
+    }
+}
